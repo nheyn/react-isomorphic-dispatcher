5603e5c79339ee6e27c1d39af59ecfabcbb88534
nheyn <nick@heyn.io>
2015-12-01 16:30:25 -0700

    Create Skeleton For Client/Server Script
    Add basic outline for the code in the scripts for the client / server.

--- example/src/server.js
+++ example/src/server.js
@@ -1,3 +1,28 @@
 /* @flow */
+import express from 'express';
 
-//TODO, start express app that sends html that loads app.js
+/*------------------------------------------------------------------------------------------------*/
+//	--- Create server ---
+/*------------------------------------------------------------------------------------------------*/
+let app = express();
+app.use('/', (req, res, next) => {
+	//TODO, add page load
+	next(new Error('NYI'));
+});
+app.use('/app.js', (req, res, next) => {
+	//TODO, add app.js load
+	next(new Error('NYI'));
+});
+app.use('/dispatch', (req, res, next) => {
+	//TODO, add isomporphic-dispatcher communication
+	next(new Error('NYI'));
+});
+app.use((err, req, res, next) => {
+	//TODO, handle error
+	console.error('[Error Handling: NYI]: ', err);
+});
+
+/*------------------------------------------------------------------------------------------------*/
+//	--- Start server ---
+/*------------------------------------------------------------------------------------------------*/
+app.listen(80);
