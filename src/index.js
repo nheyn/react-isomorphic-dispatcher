811b86239fa07c81016f16377de29efddd09445c
nheyn <nick@heyn.io>
2015-12-01 00:12:14 -0700

    Fill in 'addStoreState' Function
    Fill in the ‘AddStoreState’ higher-order component. NOTE: does not
    update w/ dispatcher

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import React from "react";
 
 type Dispatcher = any; //TODO
+type DispatcherState = any;
+type DispatcherStates = {[key: string]: DispatcherState};
 
 /*------------------------------------------------------------------------------------------------*/
 //	--- Use Dispatcher Higher-Order Componet ---
@@ -34,13 +36,31 @@ export function useDispatcher(
 }
 
 /**
- * //TODO
+ * Create a higher-order Componet that will add that state of the dispatcher (added with
+ * 'useDispatcher') to the props of the given Componet.
+ *
+ * @param Componet			The Componet to add the states to
+ * @param [storesToProps]	A function that takes the states and returns the props to add
+ *
+ * @return					The Componet with the states added
  */
-export function addStoreState(Componet: ReactClass<any, any, any>): ReactClass<any, any, any> {
+export function addStoreState(
+	Componet:		ReactClass<any, any, any>,
+	storesToProps?:	(states: DispatcherStates) => any
+):					ReactClass<any, any, any> {
 	const AddStoreState = React.createClass({
+		contextTypes: {
+			dispatcher: React.PropTypes.object.isRequired
+		},
 		render(): ReactElement {
-			//TODO
-			return <Componet {...this.props} />;
+			const storeStates = this.getStoreStates();	//TODO, change to state (to be updated)
+
+			return <Componet {...storeStates} {...this.props} />;
+		},
+		getStoreStates(): any {
+			const states = this.context.dispatcher.getStatesForAll();
+
+			return storesToProps? storesToProps(states): states;
 		}
 	});
 
