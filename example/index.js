600f7353766abd5a37986c1e2fc1311acb59f952
nheyn <nick@heyn.io>
2015-12-01 15:58:44 -0700

    Add Transpiler Code for Example
    Add code for simple Transpiling the example.

--- example/index.js
+++ example/index.js
@@ -3,8 +3,80 @@ var fs = require('fs');
 var babel = require('babel-core');
 var browserify = require('browserify');
 
-//TODO, run babel on src/
+/*------------------------------------------------------------------------------------------------*/
+//	--- Transpile source ---
+/*------------------------------------------------------------------------------------------------*/
+// Run babel on src/
+var babelPromise = runBabel(path.join(__dirname, 'src'), path.join(__dirname, 'lib'), [
+	'app.js', 'server.js'. 'client.js'
+]);
 
-//TODO, run browserify on lib/client.js
+// Run browserify on lib/client.js
+var browserifyPromise = babelPromise.then(function() {
+	return runBrowerify(path.join(__dirname, 'lib/client.js'), path.join(__dirname, 'app.js'), [
+		'express'
+	]);
+});
 
-//TODO, start server using lib/server.js
+/*------------------------------------------------------------------------------------------------*/
+//	--- Start server ---
+/*------------------------------------------------------------------------------------------------*/
+browserifyPromise.then(function() {
+	require('./lib/server.js');
+}).catch(function(err) {
+	console.error('[Transpile Error]: ', err);
+});
+
+/*------------------------------------------------------------------------------------------------*/
+//	--- Helper functions ---
+/*------------------------------------------------------------------------------------------------*/
+function runBabel(srcDir, dstDir, files) {
+	return Promise.all(files.map(function(file) {
+		return new Promise(function(resolve, reject) {
+			babel.transformFile(path.join(srcDir, file), function(err, results) {
+				if(err) {
+					reject(err);
+					return;
+				}
+
+				var writeStream = fs.createWriteStream(path.join(dstDir, file));
+				writeStream.on('error', function(err) {
+					reject(err)
+				});
+				writeStream.on('finish', function() {
+					resolve(true);
+				});
+
+				writeStream.write(results.code);
+				writeStream.end();
+			});
+		});
+	}));
+}
+
+function runBrowerify(src, dst, modulesToIgnore) {
+	return new Promise(function(resolve, reject) {
+		var b = browserify(src);
+
+		// Skip ignored modules
+		if(modulesToIgnore && modulesToIgnore.length > 0) {
+			modulesToIgnore.forEach(function(module) {
+				b.ignore(module);
+			});
+		}
+
+		// Set up bundle
+		var appBundle = b.bundle();
+		appBundle.on('error', function(err) {
+			reject(err);
+		});
+		appBundle.on('end', function() {
+			resolve(true);
+		});
+
+		// Save file
+		appBundle.pipe(
+			fs.createWriteStream(dst, { flags : 'w' })
+		);
+	});
+}
