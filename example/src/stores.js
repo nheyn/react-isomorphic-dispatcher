0062a428bb1b7e219b3a0803cb3ebf6885abe10f
nheyn <nick@heyn.io>
2015-12-01 23:15:03 -0700

    Fix Stores
    Fix the syntax error in the stores.js.

--- example/src/stores.js
+++ example/src/stores.js
@@ -1,32 +1,29 @@
 /* @flow */
 import { createStore } from 'isomorphic-dispatcher';
 
-let storeA = createStore({ value: 'a' });
+export let storeA = createStore({ value: 'a' });
 storeA = storeA.register((state, action) => {
 	if(action.type !== 'ADD_TO_A') return state;
-	return {
-		{...state}
-		count: state.count + 1
-	};
-})
-export storeA;
+	return Object.assign({},
+		state,
+		{ count: state.count? state.count+1: 1 }
+	);
+});
 
-let storeB = createStore({ value: 'b' });
+export let storeB = createStore({ value: 'b' });
 storeB = storeB.register((state, action) => {
 	if(action.type !== 'ADD_TO_B') return state;
-	return {
-		{...state}
-		count: state.count + 1
-	};
-})
-export storeB;
+	return Object.assign({},
+		state,
+		{ count: state.count? state.count+1: 1 }
+	);
+});
 
-let storeC = createStore({ value: 'c' });
+export let storeC = createStore({ value: 'c' });
 storeC = storeC.register((state, action) => {
 	if(action.type !== 'ADD_TO_C') return state;
-	return {
-		{...state}
-		count: state.count + 1
-	};
-})
-export storeC;
+	return Object.assign({},
+		state,
+		{ count: state.count? state.count+1: 1 }
+	);
+});
