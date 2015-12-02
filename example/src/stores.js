ba61f8cfd2340452613501f38e21b3716b2808ac
nheyn <nick@heyn.io>
2015-12-01 22:57:17 -0700

    Create Simple Stores
    Create three basic stores for the example site.

--- example/src/stores.js
+++ example/src/stores.js
@@ -1,3 +1,32 @@
 /* @flow */
+import { createStore } from 'isomorphic-dispatcher';
 
-//TODO, create stores to use in the examples
+let storeA = createStore({ value: 'a' });
+storeA = storeA.register((state, action) => {
+	if(action.type !== 'ADD_TO_A') return state;
+	return {
+		{...state}
+		count: state.count + 1
+	};
+})
+export storeA;
+
+let storeB = createStore({ value: 'b' });
+storeB = storeB.register((state, action) => {
+	if(action.type !== 'ADD_TO_B') return state;
+	return {
+		{...state}
+		count: state.count + 1
+	};
+})
+export storeB;
+
+let storeC = createStore({ value: 'c' });
+storeC = storeC.register((state, action) => {
+	if(action.type !== 'ADD_TO_C') return state;
+	return {
+		{...state}
+		count: state.count + 1
+	};
+})
+export storeC;
