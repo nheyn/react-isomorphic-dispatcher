0d8b0342c0871a543ae0eb040c2b7cf3e6620cb7
nheyn <nick@heyn.io>
2015-12-02 00:24:32 -0700

    Fill in App Componet
    Fill in render method of the App Component.

--- example/src/app.js
+++ example/src/app.js
@@ -10,9 +10,31 @@ const App = React.createClass({
 		dispatcher: React.PropTypes.object.isRequired
 	},
 	render(): ReactElement {
-		const Div = useDispatcher('div', this.props.dispatcher);
+		const Wrapper = useDispatcher('div', this.props.dispatcher);
+		const Title = addStoreState(SomeStoresComponent, ['storeA', 'storeB']);
+		const Summary = addStoreState(CustomPropsComponent, ['storeC'], (states) => {
+			const { value, count } = states.storeC;
+			return {
+				summary: `Store ${value} w/ count=${count}`
+			};
+		});
+		const ClickCount = addStoreState(AllStoresComponent);
+		const Clicker = addDispatch(DispatchComponent);
 
-		return <Div>App - NYI</Div>;
+		return (
+			<Wrapper>
+				<Title />
+				<Summary />
+				<table>
+					<tbody>
+						<tr>
+							<td><ClickCount /></td>
+							<td><Clicker /></td>
+						</tr>
+					</tbody>
+				</table>
+			</Wrapper>
+		);
 	}
 });
 
@@ -21,39 +43,39 @@ export default App;
 /*------------------------------------------------------------------------------------------------*/
 //	--- Some Stores Component ---
 /*------------------------------------------------------------------------------------------------*/
-const SomeStoresComponent = React.createClass({
+const SomeStoresComponent  = React.createClass({
 	render(): ReactElement {
 		//TODO
-		return <div />;
+		return <div>SomeStoresComponent</div>;
 	}
 });
 
 /*------------------------------------------------------------------------------------------------*/
 //	--- All Stores Component ---
 /*------------------------------------------------------------------------------------------------*/
-const AllStoresComponent = React.createClass({
+const AllStoresComponent  = React.createClass({
 	render(): ReactElement {
 		//TODO
-		return <div />;
+		return <div>AllStoresComponent</div>;
 	}
 });
 
 /*------------------------------------------------------------------------------------------------*/
 //	--- Custom Props Component ---
 /*------------------------------------------------------------------------------------------------*/
-const CustomPropsComponent = React.createClass({
+const CustomPropsComponent  = React.createClass({
 	render(): ReactElement {
 		//TODO
-		return <div />;
+		return <div>CustomPropsComponent</div>;
 	}
 });
 
 /*------------------------------------------------------------------------------------------------*/
 //	--- Dispatch Component ---
 /*------------------------------------------------------------------------------------------------*/
-const DispatchComponent = React.createClass({
+const DispatchComponent  = React.createClass({
 	render(): ReactElement {
 		//TODO
-		return <div />;
+		return <div>DispatchComponent</div>;
 	}
 });
